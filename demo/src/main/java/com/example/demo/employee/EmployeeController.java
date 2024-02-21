53251370f076619bc69d747ed25d37c63fe39cbb
dhanashriUsit <dhanashri.deore@usit.net.in>
2024-02-21 12:01:34 +0530

    new repo

---
+++ demo/src/main/java/com/example/demo/employee/EmployeeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.employee;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+	@GetMapping("get")
+	public String display() {
+		return "Hello Jenkins";
+	}
+}
