1c0d28c1fd35eadbbd1d01da21706c0a33276436
dhanashriUsit <dhanashri.deore@usit.net.in>
2024-02-23 12:38:54 +0530

    third

--- demo/src/main/java/com/example/demo/employee/EmployeeController.java
+++ demo/src/main/java/com/example/demo/employee/EmployeeController.java
@@ -8,8 +8,9 @@ public class EmployeeController {
 
 	@GetMapping("get")
 	public String display() {
-		return "Hello Jenkins ,How are you.888";
 		System.out.println("Hello i m here00000");
+
+		return "Hello Jenkins ,How are you.888";
 
 	}
 }
